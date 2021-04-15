209834ba71a58cfbff877ac90d0162f3ed6c9d67
Jorge <jorgediazok@gmail.com>
2021-04-15 11:34:15 -0300

    TP1 Versión final con comentarios.

--- TP1/src/utn.h
+++ TP1/src/utn.h
@@ -3,17 +3,25 @@
  *
  *  Created on: 5 abr. 2021
  *      Author: Jorge
+ *  Biblioteca de funciones
  */
 
 #ifndef UTN_H_
 #define UTN_H_
 
-//Prototipos
-
+//Esta función toma dos números enteros y los devuelve sumados
 void sumar(int A, int B);
+
+//Esta función toma dos números enteros y los devuelve restados
 void restar(int A, int B);
+
+//Esta función toma dos números enteros y los devuelve multiplicados
 void multiplicar(int A, int B);
+
+//Esta función toma dos números enteros y divide el primero con el segundo y devuelve un float
 void dividir(int A, int B);
+
+//Esta función toma dos números enteros y devuelve su factorial.
 void factorial(int A, int B);
 
 
